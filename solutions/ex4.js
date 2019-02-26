c1d4e6d5206a2858afd34221239d8498ff55e05f
Artem Molonosov <artmol91@gmail.com>
2019-02-26 20:59:51 +0100

    Exercise 4

---
+++ solutions/ex4.js
@@ -0,0 +1,7 @@
+function getShortMessages(messages) {
+  return messages
+    .filter(({ message }) => message.length < 50)
+    .map(({message}) => message);
+}
+
+module.exports = getShortMessages;
